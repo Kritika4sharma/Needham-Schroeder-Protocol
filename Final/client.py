958bab2069ccd5c1b7d95d2942f8c9c2d061dd65
Kritika sharma <ks4347630@gmail.com>
2018-04-12 20:04:11 +0530

    Perfectly running code between 1 kdc and 3 clients implemented

--- Final/client.py
+++ Final/client.py
@@ -11,22 +11,26 @@ import bisect
 import threading
 import random
 
+NAME = input("Enter your name\n")
+
 
 class Server :
-	def __init__(self,kdc_port,c1_port,c2_port) :
+	def __init__(self,kdc_port,Alice_port,Bob_port,Cherry_port) :
 
-		self.MY_IP = '172.21.21.105'
-		self.kdc_ip = '172.21.21.105'
-
+		self.MY_IP = '172.21.21.201'
+		self.kdc_ip = '172.21.21.201'
 		self.socket_obj = {}
-
 		self.HOST = self.MY_IP
-		self.peer_port = int(c1_port)
 		self.kdc_port = int(kdc_port)
-		self.c2_port = int(c2_port)
-
-		self.bind_and_serve()           # communication with peers and clients after server creation
-
+		self.Alice_port = Alice_port
+		self.Bob_port = Bob_port
+		self.Cherry_port = Cherry_port
+		self.peer_port = int(Alice_port)
+		if(NAME=="Bob"):
+			self.peer_port = int(Bob_port)
+		if(NAME=="Cherry"):
+			self.peer_port = int(Cherry_port)
+		self.bind_and_serve()           
 		print ('Super Outside')
 		
 
@@ -34,19 +38,33 @@ class Server :
 		global BUFFER, count
 		conn = buff[0]
 		msg = conn.recv(1024)
-		msg = msg.decode()
 		print (msg)
-		inp = "Hii peer"
-		inp = inp.encode()
-		conn.send(inp)
+
+		key = input("Enter Your key for Decryption")
+		obj1 = ARC4.new(key)
+		msg = obj1.decrypt(msg)
+		print("Your Decrypted Ticket")
+		print(msg)
+		
+		keys = msg.split(b'-')
+		session_key = keys[1]
+		nonceB = random.randint(1,10000000)
+		nonceB = str(nonceB)
+		nonceB = nonceB.encode()
+		print(nonceB)
+		obj2 = ARC4.new(session_key)
+		nonceB = obj2.encrypt(nonceB)
+		conn.send(nonceB)
+		msg = conn.recv(1024)
+		nonceB = obj2.decrypt(msg)
+		print ("Number is : ",nonceB)
 		print ("Two clients chatting")
 		conn.close()
-
 
 	def checking(self):
 		while(True):
 			print ("Want to talk to someone??")
-			inp = input("yes/no???\n")
+			inp = input("yes/no??")
 			if(inp=="yes"):
 				self.talk_to_someone(self.kdc_port)
 
@@ -59,7 +77,7 @@ class Server :
 		msg = s.recv(1024)
 		obj1 = ARC4.new(session_key)
 		msg = obj1.decrypt(msg)
-		print("Decrypted Nonce")
+		print("Decrypted Nonce : ",msg)
 		msg = msg.decode()
 		msg = int(msg)
 		msg = msg -1
@@ -68,52 +86,51 @@ class Server :
 		nonceA = obj1.encrypt(msg)
 		s.send(nonceA)
 
-
-	
 	def talk_to_someone(self,kdc_port):
 		host = self.kdc_ip
 		port = self.kdc_port 
 		s = socket.socket()             # Create a socket object
 		s.connect((host, port))
+		print ("Give input in format : Initiator-Responder")
 		inp = input()
 		inp = inp.split('-')
 		initiator = inp[0]
 		responder = inp[1]
+		self.c2_port = int(self.Alice_port)
+		if(responder=="Bob"):
+			self.c2_port = int(self.Bob_port)
+		if(responder=="Cherry"):
+			self.c2_port = int(self.Cherry_port)
 		nonceA = str(random.randint(1,1000000000))
 		message =  initiator + '-' + responder  + '-' + nonceA
 		message = message.encode()
-		print("Message Sent")
+		print("Message Sent to kdc")
 		print(message)
 		s.send(message) 
 
 		complete_ticket = s.recv(4096)
-		print("Complete Ticket Received to Alice")
+		print("Complete Ticket Received to ",initiator)
 		print(complete_ticket)
 		key = input("Enter your key for decryption of ticket :")
 		obj1 = ARC4.new(key)
 		alice_ticket = obj1.decrypt(complete_ticket)
 		tickets = alice_ticket.split(b'$$@@')
-		print("Decrypted Ticket by Alice")
+		print("Decrypted Ticket by ",initiator)
 		print(tickets)
 		alice_ticket = tickets[0]
-		print("Alice Ticket")
+		print("Ticket of ",initiator)
 		print(alice_ticket)
 		alice_ticket = alice_ticket.split(b'-')
 		session_key = alice_ticket[2]
 		bob_ticket_original = tickets[1]
-		key = 'Bob@123'
-		obj2 = ARC4.new(key)
-		bob_ticket = obj2.decrypt(bob_ticket_original)
-		print("Bob Ticket")
-		print(bob_ticket)
+		print("Ticket of ",responder)
+		print(bob_ticket_original)
 		s.close()
 		print('connection closed')
 
 		self.chatting(bob_ticket_original,session_key)
 
 	def bind_and_serve(self):
-		print ("Client Module Started...")
-
 		Thread(target=self.checking, args=()).start()     
 		self.socket_obj.update({'s' : socket.socket(socket.AF_INET, socket.SOCK_STREAM)})  
 																						
@@ -128,7 +145,7 @@ class Server :
         	
 
 def main() :
-	server_obj = Server(sys.argv[1],sys.argv[2],sys.argv[3])
+	server_obj = Server(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
 
 
 if __name__ == "__main__" :
--- Final/server.py
+++ Final/server.py
@@ -8,12 +8,13 @@ import random
 from Crypto.Cipher import AES  
 from Crypto.Cipher import ARC4
 
-my_ip = '172.21.21.105'
+my_ip = '172.21.21.201'
 
 users = []
 UserA = {'id': 'A' , 'name' : 'Alice','key':'Alice@123'}
 UserB = {'id': 'B' , 'name' : 'Bob','key':'Bob@123'}
 UserC = {'id': 'C' , 'name' : 'Cherry','key':'Cherry@123'}
+
 
 users.append(UserA)
 users.append(UserB)
@@ -30,16 +31,34 @@ def client_thread(buff):
 	initiator = msg[0]
 	responder = msg[1]
 	nonceA = msg[2]
+	if initiator == 'Alice':
+		initiator_key = UserA['key']
+
+	if initiator == 'Bob':
+		initiator_key = UserB['key']
+
+	if initiator == 'Cherry':
+		initiator_key = UserC['key']
+
+	if responder == 'Alice':
+		responder_key = UserA['key']
+
+	if responder == 'Bob':
+		responder_key = UserB['key']
+
+	if responder == 'Cherry':
+		responder_key = UserC['key']
+
 	print("Ticket Sent to !!" , initiator)
 	session_key = str(random.randint(1,1000000000))
 	ticket_responder =  initiator + '-' + session_key
 	ticket_responder = ticket_responder.encode()
-	obj1 = ARC4.new(UserB['key'])
+	obj1 = ARC4.new(responder_key)
 	ticket_responder = obj1.encrypt(ticket_responder)
 	final_ticket = (nonceA + '-' + responder + '-' + session_key )
 	final_ticket = final_ticket.encode()
 	final_ticket = final_ticket +  b'$$@@' + ticket_responder
-	obj2 = ARC4.new(UserA['key'])
+	obj2 = ARC4.new(initiator_key)
 	final_ticket = obj2.encrypt(final_ticket)
 	conn.send(final_ticket)
 
